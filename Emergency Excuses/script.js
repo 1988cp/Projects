2ad6240a2ca19bd2a16869e67f49604649a512a0
Chris <1988cpaul@gmail.com>
2022-04-10 22:14:12 +0100

    removed trial code

--- Emergency Excuses/script.js
+++ Emergency Excuses/script.js
@@ -1,59 +1,3 @@
-
-/*const url = "https://excuser.herokuapp.com/v1/excuse"
-
-async function start(url) {
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
-        
-    } catch (err) {
-        console.log("There was a problem fetching the excuse list. Ironically, we have no reason to give you.")
-    }
-
-}
-
-excuseBtn.addEventListener('click', function () {
-    start()
-    console.log(writeExcuse)})
-
-*/
-
-/* 
-
-fetch('https://excuser.herokuapp.com/v1/excuse')
-.then (res => res.json)
-.then (data => console.log(data))
-
-*/
-
-
-/*
-async function loadExcuse() {
-    try {
-        const response = await fetch("https://excuser.herokuapp.com/v1/excuse")
-        const excuse = await response.json()
-        console.log(excuse)
-        //return excuse
-    } catch (error) {
-        console.log('No excuse - we could not load your excuses')
-    }
-}
-
-excuseBtn.addEventListener('click', function (){
-    loadExcuse()
-    //const excuse = excuse;
-
-    //document.querySelector('.excuse').textContent = loadExcuse;
-    //document.querySelector('.excuse').textContent = excuse;
-    //document.querySelector('.excuse').textContent = `${excuse}`;
-})
-
-
-
-//console.log(excuse)
-
-*/
-
 const excuse = document.querySelector('.excuse')
 
 const excuseBtn = document.querySelector('.excuseBtn')
@@ -65,8 +9,6 @@ excuseBtn.addEventListener('click', async function loadExcuse(){
             const excuse = data[0].excuse
             console.log(excuse)
             document.querySelector('.excuse').textContent = excuse
-            //document.querySelector('.excuse').textContent = excuse[0]
-            //return excuse
         } catch (error) {
             console.log('No excuse - we could not load your excuses')
         }
